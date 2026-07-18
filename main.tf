d72b46636f38767ca09c5112e89d6b89597a176d
Divya Shukla <70107635+divyashukla23@users.noreply.github.com>
2026-07-18 11:15:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,35 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -18,5 +47,38 @@ resource "aws_instance" "web" {
   tags = {
     Name        = var.instance_name
     Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "demo-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "tg_attach" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web[count.index].id
+  port             = 80
+}
+
+
+resource "aws_lb" "alb" {
+  name               = "demo-alb"
+  load_balancer_type = "application"
+  subnets            = var.subnets
+  security_groups    = [aws_security_group.web_sg.id]
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
   }
 }
